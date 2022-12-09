5c1461e1873b82da00dc70909a434cbbd6b932aa
Yogesh Bhat <yogesh99bhat@gmail.com>
2022-12-09 11:51:37 +0530

    Added user controller

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+// Redirect the request to neighbor if url contains /users
+router.use('/users',require('./users'));
 
 
 module.exports = router;
