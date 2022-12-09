0967e0dcaa078182caf47ab45e6925f70c72d2ed
Yogesh Bhat <yogesh99bhat@gmail.com>
2022-12-10 01:36:10 +0530

    Added styles to header and footer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,4 +3,16 @@ module.exports.profile = function (req, res) {
     return res.render('profile', {
         title: "User | Profile",
     })
-}
+};
+
+module.exports.sign_in = function(req,res){
+    return res.render('user_signin',{
+        title: "Sign In",
+    })
+};
+
+module.exports.sign_up = (req,res) =>{
+    return res.render('user_signup',{
+        title: "Sing Up",
+    })
+};
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/users_controller");
 
 router.get('/profile', userController.profile);
+router.get('/signin', userController.sign_in);
+router.get('/signup', userController.sign_up);
 
 module.exports = router;
