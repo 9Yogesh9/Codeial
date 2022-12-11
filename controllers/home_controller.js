ed99f5d13fae7cfc730a28e3157b386fddd76aa9
Yogesh Bhat <yogesh99bhat@gmail.com>
2022-12-12 01:07:28 +0530

    rendered posts with the name of creators

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,26 @@
-module.exports.home = function(req,res){
-    return res.render('home',{
-        title: "Codeial | Home",
+const Post = require('../models/post');
+const User = require('../models/user');
+
+module.exports.home = function (req, res) {
+
+    // This code was sending just the user ID linked to Posts
+    // Post.find({},(err,posts)=>{
+    //     if(err){console.log(`Error in finding the posts to render : ${post}`); return;}
+
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     })
+    // })
+
+    // Prepopulating the user before sending to browser
+    Post.find({}).populate('user').exec((err, posts) => {
+        if (err) { console.log(`Error in finding the posts to render : ${post}`); return; }
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        })
     })
+
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const postController = require('../controllers/posts_controller');
 
-router.post('/createposts', postController.create_post);
+router.post('/createposts', passport.checkAuthentication, postController.create_post);
 
 module.exports = router;
